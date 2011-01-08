d33b21b6f579ac72afd038b2ddbf1a672e9bc940
Vlad Ureche <vlad.ureche@gmail.com>
2011-01-08 03:19:12 +0100

    Complete (and correct) AST generation

--- src/internal/Scheduling.scala
+++ src/internal/Scheduling.scala
@@ -21,6 +21,7 @@ trait Scheduling {
 
 
   def dep(e: Exp[Any]): List[Sym[Any]] = e match { // only used by GraphVizExport currently
+    case RichDef(used) => used.flatMap(t => syms(t))
     case Def(d: Product) => syms(d)
     case _ => Nil
   }
