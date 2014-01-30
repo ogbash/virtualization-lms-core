01476fa7225d9db6c3df8c5c9de475b090cf08c2
Arvind Sujeeth <asujeeth@stanford.edu>
2014-01-30 00:37:08 -0800

    remove unnecessary printStackTrace exception handler for Scala codegen

--- src/internal/ScalaCodegen.scala
+++ src/internal/ScalaCodegen.scala
@@ -15,12 +15,6 @@ trait ScalaCodegen extends GenericCodegen with Config {
 
   override def toString = "scala"
 
-  override def exceptionHandler(e: Exception, outFile:File, kstream:PrintWriter): Unit = {
-      e.printStackTrace()
-      kstream.close()
-      outFile.delete
-  }
-
   def emitSource[A : Manifest](args: List[Sym[_]], body: Block[A], className: String, out: PrintWriter) = {
 
     val sA = remap(manifest[A])
@@ -36,18 +30,18 @@ trait ScalaCodegen extends GenericCodegen with Config {
       // TODO: separate concerns, should not hard code "pxX" name scheme for static data here
       stream.println("class "+className+(if (staticData.isEmpty) "" else "("+staticData.map(p=>"p"+quote(p._1)+":"+p._1.tp).mkString(",")+")")+" extends (("+args.map(a => remap(a.tp)).mkString(", ")+")=>("+sA+")) {")
       stream.println("def apply("+args.map(a => quote(a) + ":" + remap(a.tp)).mkString(", ")+"): "+sA+" = {")
-    
+
       emitBlock(body)
       stream.println(quote(getBlockResult(body)))
-    
+
       stream.println("}")
-    
+
       stream.println("}")
       stream.println("/*****************************************\n"+
                      "  End of Generated Code                  \n"+
                      "*******************************************/")
     }
-    
+
     staticData
   }
 
@@ -97,27 +91,27 @@ trait ScalaCodegen extends GenericCodegen with Config {
     }
     stream.println("val " + quote(sym) + " = " + rhs + extra)
   }
-  
+
   def emitVarDef(sym: Sym[Variable[Any]], rhs: String): Unit = {
     stream.println("var " + quote(sym) + ": " + remap(sym.tp) + " = " + rhs)
   }
-  
+
 }
 
 trait ScalaNestedCodegen extends GenericNestedCodegen with ScalaCodegen {
   val IR: Expressions with Effects
   import IR._
-  
+
   // emit forward decls for recursive vals
   override def traverseStmsInBlock[A](stms: List[Stm]): Unit = {
     recursive foreach emitForwardDef
     super.traverseStmsInBlock(stms)
   }
-  
+
   def emitForwardDef(sym: Sym[Any]): Unit = {
     stream.println("var " + quote(sym) + /*": " + remap(sym.tp) +*/ " = null.asInstanceOf[" + remap(sym.tp) + "]")
   }
-  
+
   // special case for recursive vals
   override def emitValDef(sym: Sym[Any], rhs: String): Unit = {
     if (recursive contains sym)
@@ -125,14 +119,14 @@ trait ScalaNestedCodegen extends GenericNestedCodegen with ScalaCodegen {
     else
       super.emitValDef(sym,rhs)
   }
-  
+
 }
 
 
 trait ScalaFatCodegen extends GenericFatCodegen with ScalaCodegen {
   val IR: Expressions with Effects with FatExpressions
   import IR._
-  
+
   def emitKernelExtra(syms: List[Sym[Any]]): Unit = {
     val kernelName = syms.map(quote).mkString("")
     stream.println("final class activation_" + kernelName + " {")
@@ -141,7 +135,7 @@ trait ScalaFatCodegen extends GenericFatCodegen with ScalaCodegen {
     }
     stream.println("}")
   }
-  
+
   override def emitFatNodeKernelExtra(syms: List[Sym[Any]], rhs: FatDef): Unit = emitKernelExtra(syms)
   override def emitNodeKernelExtra(syms: List[Sym[Any]], rhs: Def[Any]): Unit = emitKernelExtra(syms)
 
